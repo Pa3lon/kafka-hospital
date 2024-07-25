d12fe52714dc6714cbd404ea3715d468515f98e7
Patrick Lønhaug <patrick.lonhaug@nhn.no>
2024-07-25 21:41:17 +0200

    Init

---
+++ components/Tabs.vue
@@ -0,0 +1,8 @@
+<script setup>
+const tabSelected = useState("tab", () => 0);
+const items = [{ label: "Patients" }, { label: "Staff" }, { label: "Rooms" }];
+</script>
+
+<template>
+  <UTabs v-model="tabSelected" :items="items"> </UTabs>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen flex justify-center text-black">
+    <div class="w-8/12">
+      <Tabs />
+      <div>
+        {{ tabSelected }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const tabSelected = useState("tab", () => 2);
+</script>
